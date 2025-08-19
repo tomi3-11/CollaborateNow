29aed07659a7a0a31137f997a3356dd1a85a3284
thomas <didymusthomas6@gmail.com>
2025-08-19 17:06:22 +0300

    Admin Project editing functionality implemented

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Project, ProjectMembership, UserProfile, ProjectRequiredSkill, Skill
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 # Register your models here.
 
@@ -13,12 +15,21 @@ admin.site.register(Skill)
 class ProjectSkillRequiredInline(admin.TabularInline):
     model = ProjectRequiredSkill
     extra = 1
+    
+    
+class ProjectMembershipInline(admin.TabularInline):
+    model = ProjectMembership
+    extra = 1 # number of empty forms to display for adding new members
+    raw_id_fields = ('user',) # User raw_id_fields for better performance with many users
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'creator', 'created_at', 'status')
-    list_filter = ('status',)
-    actions = ['approve_projects', 'reject_projects']
+    list_display = ('title', 'creator', 'created_at', 'status', 'max_members', 'current_members')
+    list_filter = ('status', 'creator', 'created_at')
+    actions = ['approve_projects', 'reject_projects', 'email_members']
+    inlines = [ProjectSkillRequiredInline, ProjectMembershipInline]
+    list_editable = ('status', 'max_members')
+    readonly_fields = ('current_members',) # Making the current_members read-only
 
     def approve_projects(self, request, queryset):
         queryset.update(status='approved')
@@ -31,5 +42,27 @@ class ProjectAdmin(admin.ModelAdmin):
         self.message_user(request, f"{queryset.count()} project(s) rejected.")
 
     reject_projects.short_description = "Rejected selected projects."
+    
+    """
+    Implementing sending emails with a simple message to the console
+    Further Implementation will be for the real time email sending
+        - Django and SMTP( Simple Mail Transfer Protocol)
+    
+    """
+    def email_members(self, request, queryset):
+        for project in queryset:
+            memberships = ProjectMembership.objects.filter(project=project).select_related('user')
+            recipient_list = [membership.user.email for membership in memberships if membership.user.email]
+            if recipient_list:
+                # In a real application, This is where the email will be sent
+                print(f"Sending email to members of '{project.title}': {recipient_list}")
+                self.message_user(request, f"Email initiated to {len(recipient_list)} members of '{project.title}'.")
+            else:
+                self.message_user(request, f"No members found for '{project.title}'.")
+    email_members.short_description = "Email selected project members"
 
 admin.site.register(Project, ProjectAdmin)
+
+class ProjectMembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'project', 'joined_date')
+admin.site.register(ProjectMembership, ProjectMembershipAdmin) # keep the separate admin for ProjectMembership as well.
