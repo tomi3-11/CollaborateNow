84ea24aede88b902104bb7c46fcfdf11fd814ede
thomas <didymusthomas6@gmail.com>
2025-08-29 04:20:04 +0300

    Task forms created

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Project, UserProfile, ProjectRequiredSkill
+from .models import Project, UserProfile, ProjectRequiredSkill, Task, User
 
 class RegistrationForm(UserCreationForm):
     class Meta:
@@ -149,5 +149,41 @@ class CreateProjectStep3Form(forms.ModelForm):
             'deadline': forms.DateInput(attrs={'type': 'date'}, format="%Y-%m-%d"),
         }
         
+
+# Task form
+class TaskForm(forms.ModelForm):
+    due_date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={
+        'type': 'datetime-local'
+    }), required=False)
+    assigned_to = forms.ModelChoiceField(queryset=None, required=False, label="Assign To")
+    status = forms.ChoiceField(choices=Task.status_choices, widget=forms.Select(attrs={'class': 'form-control'}))
     
-    
+    class Meta:
+        model = Task
+        fields = [
+            'title', 'description', 'assigned_to', 'due_date', 'status',
+        ]
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3})
+        }
+        
+    def __init__(self, *args, **kwargs):
+        project = kwargs.pop('project', None)
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if project:
+            self.fields['assigned_to'].queryset = project.projectmembership_set.select_related('user').values_list('user', flat=True)
+        else:
+            self.fields['assigned_to'].queryset = User.objects.none()
+        
+        if user:
+            self.fields['assigned_to'].initial = user
+
+
+TaskFormSet = inlineformset_factory(
+    Project,
+    Task,
+    form=TaskForm,
+    extra=1,
+    can_delete=True
+)    
